5c91786b4fd3071ef6cac18019095ca9fa7a610e
Krys Pinto <pinto.krysler@gmail.com>
2017-03-02 21:24:37 -0700

    Added a better description of myself

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
 	gulp.src('scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('public/css/'))
+				.pipe(browserSync.reload({
+		      stream: true
+		    }))
 });
 
 gulp.task('styles:watch', function () {
   gulp.watch('scss/**/*.scss', ['styles']);
 });
 
-gulp.task('start', ['styles', 'styles:watch']);
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'public/views/home.html'
+    },
+  })
+})
+
+
+gulp.task('start', ['browserSync','styles', 'styles:watch']);
